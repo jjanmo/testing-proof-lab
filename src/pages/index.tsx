c2eec22d6b711cfa5e957643488ccb0af740b72e
jjanmo <jjanmo07@hanmail.net>
2024-12-27 14:11:13 +0900

    refactor: reset project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
-export default function Home() {
-  return (
-    <main>
-      <h1 className="text-9xl">Next 배포 테스트</h1>
-    </main>
-  );
-}
+const Home = () => {
+  return <div>Hello World</div>;
+};
+
+export default Home;
